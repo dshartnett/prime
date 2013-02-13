2b6152065725d3a6319c32d8c23382b91b987182
U-David-HP\David <David@David-HP.(none)>
2013-02-13 17:08:39 -0600

    prime

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // David H.
-window['Prime']={};
+window['Prime'] = {};
 
 (function(){
 
@@ -11,48 +11,44 @@ FRAME_MAX: 30,
 CANVAS_WIDTH: 1024,
 CANVAS_HEIGHT: 640,
 SPEED_SLIDER_MAX: 1000,
+MAX_COUNT: 3200*3200
 };
 
 window.requestAnimFrame = (function () {
  return window.requestAnimationFrame
- 					|| window.webkitRequestAnimationFrame
- 					|| window.mozRequestAnimationFrame
- 					|| window.oRequestAnimationFrame
- 					|| window.msRequestAnimationFrame
- 					|| function(callback){window.setTimeout(callback, 1000 / CONST.FPS);};
+	|| window.webkitRequestAnimationFrame
+	|| window.mozRequestAnimationFrame
+	|| window.oRequestAnimationFrame
+	|| window.msRequestAnimationFrame
+	|| function(callback){window.setTimeout(callback, 1000 / CONST.FPS);};
 }
 )();
 
 function Ulam_Spiral(x,y,dot_size,dot_color)
 {
-	var pos_x = x,
-		pos_y = y,
-		dir_x = 1,
+	var dir_x = 1,
 		dir_y = 0,
 		count = 1,
-		size = dot_size,
-		color = dot_color,
 		line_size = 1,
 		line_count = 1,
-		line_size_change = 1;
+		line_size_change = 1,
+		kill_me = false;
 	
 	function is_prime(x)
 	{
 		var sqrt_x = Math.ceil(Math.sqrt(x));
-		if (x%2==0 && x!=2 || x==1) return false;
-		for (var i = 3; i <= sqrt_x; i++)
-		{
-			if (x%i==0) return false;
-		}
+		if (!(x&1) && x!=2 || x==1) return false;
+		for (var i = 3; i <= sqrt_x; i+=2) if (x%i==0) return false;
 		return true;
 	}
 	
+	this.kill = function(){return kill_me;};
+	
 	this.next_step = function(context){
-		//console.log(line_size + " " + count + " " + x + " " + y + " " + dir_x + " " + dir_y + " " + line_size_change + " " + line_count + " " + is_prime(count));
-		if (x > -dot_size && x < CONST.CANVAS_WIDTH && y > -dot_size && y < CONST.CANVAS_HEIGHT && is_prime(count))// && (is_prime(count-2) || is_prime(count+2)))
+		if (is_prime(count) && x > -dot_size && x < CONST.CANVAS_WIDTH && y > -dot_size && y < CONST.CANVAS_HEIGHT)
 		{
-			context.fillStyle = color;
-			context.fillRect(x,y,size,size);
+			context.fillStyle = dot_color;
+			context.fillRect(x,y,dot_size,dot_size);
 		}
 		
 		x += dir_x*dot_size;
@@ -76,6 +72,7 @@ function Ulam_Spiral(x,y,dot_size,dot_color)
 			line_count = line_size;
 			line_size_change--;
 		}
+		if (count > CONST.MAX_COUNT) kill_me = true;
 	};
 }
 
@@ -97,23 +94,25 @@ function Toy()
 
 	this.initialize = function () {
 		$(document).ready(function(){
-			CONST.CANVAS_WIDTH = window.screen.availWidth - 10;
-			CONST.CANVAS_HEIGHT = window.screen.availHeight - 160;
+			CONST.CANVAS_WIDTH = window.screen.availWidth;
+			CONST.CANVAS_HEIGHT = window.screen.availHeight;
 
 			main_canvas = $("<canvas id='main_canvas' width='" + CONST.CANVAS_WIDTH + "' height='" + CONST.CANVAS_HEIGHT + "'>Update your browser</canvas>");
 			main_context = main_canvas.get(0).getContext('2d');
+
+			$("<table id='table'><tbody><tr id='row_1'></tr><tr id='row_2'></tr></tbody></table>")['appendTo']("body");
+			$("<td><button name='Clear' type='button' onclick='Prime.clear()'>Clear</button></td><td><div id='slider_dot_size'></div></td><td><div id='slider_speed'></div></td>")['appendTo']("#row_1");
+			$("<td></td><td id='dot_size'>Dot size</td><td id='speed'>Draw speed</td>")['appendTo']("#row_2");
+			$("<h1>Click anywhere below to generate <a href='http://en.wikipedia.org/wiki/Ulam_spiral'>Ulam spirals</a>.</h1>")['appendTo']("body");
+			
 			main_canvas['appendTo']("body");
 			$('#main_canvas').click(function(e) {
 				var x = e.pageX - this.offsetLeft;
 				var y = e.pageY - this.offsetTop;
 				spirals[spiral_num++] = new Ulam_Spiral(x,y,dot_size,'black');
 			});
-
-			$("<table id='table'><tbody><tr id='row_1'></tr><tr id='row_2'></tr></tbody></table>")['appendTo']("body");
-			$("<td><button name='Clear' type='button' onclick='Prime.clear()'>Clear</button></td><td><div id='slider_dot_size'></div></td><td><div id='slider_speed'></div></td>")['appendTo']("#row_1");
-			$("<td></td><td id='dot_size'>Dot size</td><td id='speed'>Draw speed</td>")['appendTo']("#row_2");
 			
-			Prime['clear'] = function()
+			window['Prime']['clear'] = function()
 			{
 				for (var u in spirals) delete spirals[u];
 				spiral_num = 0;
@@ -134,7 +133,11 @@ function Toy()
 	function draw() {
 		request_id = window.requestAnimFrame(draw);
 		var then = Date.now(), i = 0;
-		while (Date.now() - then < interval && (draw_per_frame == CONST.SPEED_SLIDER_MAX || i++ < draw_per_frame)) for (var u in spirals) spirals[u].next_step(main_context);
+		while (Date.now() - then < interval && (draw_per_frame == CONST.SPEED_SLIDER_MAX || i++ < draw_per_frame))
+			for (var u in spirals){
+				spirals[u].next_step(main_context);
+				if (spirals[u].kill()) delete spirals[u];
+			}
 	};
 
 	return this;
@@ -143,5 +146,4 @@ function Toy()
 var main = new Toy();
 
 main.initialize();
-
 })();
